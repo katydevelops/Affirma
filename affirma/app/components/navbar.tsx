6f955326d08361077b19314f6be5c372063e287e
Katy Senia <katy.develops@gmail.com>
2024-08-11 15:04:26 -0400

    create navbar

---
+++ affirma/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/feed">Feed</Link></li>
+        <li><Link href="/profile">Profile</Link></li>
+      </ul>
+    </nav>
+  );
+}
