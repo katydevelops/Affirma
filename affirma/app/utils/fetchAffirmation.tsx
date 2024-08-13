4313a2e98f5765538354fa24ab81dd39baed7283
Katy Senia <katy.develops@gmail.com>
2024-08-13 18:32:38 -0400

    update fetch affirmation

--- affirma/app/utils/fetchAffirmation.tsx
+++ affirma/app/utils/fetchAffirmation.tsx
@@ -1,19 +1,16 @@
-const API_URL = 'https://www.affirmations.dev';
+const API_ROUTE = '/api/affirmation';
 
 export async function getAffirmation() {
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(API_ROUTE);
     if (!response.ok) {
-    console.error('Fetch failed:', response);
-    throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
     return data;
   } catch (error) {
     if (error instanceof Error) {
-      // Type assertion to check if error is of type Error
       console.error('Network or fetch error:', error.message);
-      console.error('Error stack trace:', error.stack);
     } else {
       console.error('Unknown error occurred:', error);
     }
