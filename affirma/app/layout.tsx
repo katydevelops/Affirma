677e4d0eca0996db633270334731b6ae26f6ad3f
Katy Senia <katy.develops@gmail.com>
2024-08-14 20:07:45 -0400

    update layout.tsx structure

--- affirma/app/layout.tsx
+++ affirma/app/layout.tsx
@@ -1,13 +1,20 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { ChakraProvider } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ChakraProvider>
+      <html lang="en">
+        <body>
+          <div id="root">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ChakraProvider>
   );
 }
