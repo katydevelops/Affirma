eea55019d8bcefdf6024daa20b45b81c1dbeab82
Katy Senia <katy.develops@gmail.com>
2024-08-14 20:15:26 -0400

    add error boundary to test ui state issue

--- affirma/app/page.tsx
+++ affirma/app/page.tsx
@@ -1,16 +1,12 @@
-"use client";
-
-import { useEffect, useState } from 'react';
-import { getAffirmation } from './utils/getAffirmation';
-import Affirmation from './components/affirmation';
 import { Box, VStack, Text, Textarea, Button, Input } from '@chakra-ui/react';
+import ErrorBoundary from './components/ErrorBoundary';
+import Affirmation from './components/affirmation';
 
 export default function Home() {
-  //const [affirmation, setAffirmation] = useState<string>('');
   const [journalEntry, setJournalEntry] = useState<string>('');
   const [submitted, setSubmitted] = useState<boolean>(false);
 
- const handleSubmit = () => {
+  const handleSubmit = () => {
     setSubmitted(true);
     // Add functionality to save the journal entry
   };
@@ -26,51 +22,51 @@ export default function Home() {
   }
 
   return (
-    <Box
-      minH="100vh"
-      bgGradient="linear(to-r, coral.100, orange.100, pink.100)"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      p={4}
-    >
-      <VStack
-        spacing={8}
-        align="center"
-        bg="white"
-        p={8}
-        borderRadius="lg"
-        boxShadow="xl"
-        maxW="lg"
-        mx="auto"
+    <ErrorBoundary>
+      <Box
+        minH="100vh"
+        bgGradient="linear(to-r, pink.100, orange.100, pink.100)"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        p={4}
       >
-      <Affirmation />
-      
-      <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
-        <Textarea
-            value={journalEntry}
-            onChange={(e) => setJournalEntry(e.target.value)}
-            placeholder="Write your thoughts here..."
-            size="lg"
-            resize="none"
-            bg="orange.50"
-            borderColor="orange.300"
-            focusBorderColor="orange.500"
-            _placeholder={{ color: 'orange.300' }}
-        />
-        {/* Add fields for photo upload and emoji selection in the future */}
-        <Input
-            placeholder="Upload a photo (optional)"
-            type="file"
-            borderColor="orange.300"
-            my={4}
-            _focus={{ borderColor: 'orange.500' }}
-        />
-          <Button colorScheme="orange" size="md" type="submit">
-          Submit
-        </Button>
-      </form>
-    </VStack>
-    </Box>
+        <VStack
+          spacing={8}
+          align="center"
+          bg="white"
+          p={8}
+          borderRadius="lg"
+          boxShadow="xl"
+          maxW="lg"
+          mx="auto"
+        >
+          <Affirmation />
+          <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
+            <Textarea
+              value={journalEntry}
+              onChange={(e) => setJournalEntry(e.target.value)}
+              placeholder="Write your thoughts here..."
+              size="lg"
+              resize="none"
+              bg="orange.50"
+              borderColor="orange.300"
+              focusBorderColor="orange.500"
+              _placeholder={{ color: 'orange.300' }}
+            />
+            <Input
+              placeholder="Upload a photo (optional)"
+              type="file"
+              borderColor="orange.300"
+              my={4}
+              _focus={{ borderColor: 'orange.500' }}
+            />
+            <Button colorScheme="orange" size="md" type="submit">
+              Submit
+            </Button>
+          </form>
+        </VStack>
+      </Box>
+    </ErrorBoundary>
   );
 }
