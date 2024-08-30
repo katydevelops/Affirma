08010fa709821cff37caec7451d721fa282b615e
Katy Senia <katy.develops@gmail.com>
2024-08-29 20:30:48 -0400

    update getAffirmation to reinforce timestamp to prevent caching

--- app/api/affirmation/route.ts
+++ app/api/affirmation/route.ts
@@ -3,14 +3,19 @@ import { NextResponse } from 'next/server';
 const API_URL = 'https://www.affirmations.dev';
 
 export async function GET() {
+  console.log("Fetching a new affirmation from the external API...");
   try {
     const response = await fetch(API_URL);
+    const data = await response.json();
+    
+    console.log("Received data from external API:", data);
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json();
     return NextResponse.json(data);
   } catch (error) {
+    console.error('Error fetching affirmation:', error);
     return NextResponse.json({ error: 'Failed to fetch affirmation' }, { status: 500 });
   }
 }
--- app/utils/getAffirmation.tsx
+++ app/utils/getAffirmation.tsx
@@ -1,6 +1,6 @@
 export async function getAffirmation() {
-  const response = await fetch(`/api/affirmation?time=${new Date().getTime()}`, {
-    cache: 'no-store'
+  const response = await fetch(`/api/affirmation?timestamp=${new Date().getTime()}`, {
+    cache: 'no-store', 
   });
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
