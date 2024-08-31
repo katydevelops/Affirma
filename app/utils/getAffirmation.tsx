5493b1c5f410da7ba9f455f6f07f6eee4eebf9f6
Katy Senia <katy.develops@gmail.com>
2024-08-31 11:55:10 -0400

    add force caching to getAffirmation util

--- app/utils/getAffirmation.tsx
+++ app/utils/getAffirmation.tsx
@@ -1,6 +1,12 @@
 export async function getAffirmation() {
   const response = await fetch(`/api/affirmation?timestamp=${new Date().getTime()}`, {
-    cache: 'no-store', 
+    method: 'GET',
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache', // Fallback for older HTTP/1.0 caches
+      'Expires': '0',       // Another fallback
+    },
+    cache: 'no-store',       // Ensure fetch itself doesn't cache the response
   });
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
